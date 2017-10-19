d2192aac8d3cc9f6429d8e2207e80162713b7197
Quazi Tausif Ahmed <quazi.tausif@rediffmail.com>
2017-10-19 18:42:08 +0530

    Basic cookie initialization(should use Hash function for Security)

---
+++ 1.Local_Host/4.Cookie/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+var un string
+
+func init() {
+	tpl = template.Must(template.ParseGlob("WebPage/*.gohtml"))
+}
+
+func main()  {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/data", data)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.ListenAndServe(":8015", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		http.Error(w, "We are not sure what happened!", http.StatusNotFound)
+	}
+}
+
+func data(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie(un)
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	err = tpl.ExecuteTemplate(w, "data.gohtml", c)
+	if err != nil {
+		http.Error(w, "We are not sure what happened!", http.StatusNotFound)
+	}
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	un = req.FormValue("uname")
+	up := req.FormValue("upass")
+	if up == un {
+		http.SetCookie(w, &http.Cookie{
+			Name:  un,
+			Value: "Cookie Is Activated",
+		})
+		http.Redirect(w, req, "/data", http.StatusSeeOther)
+	}
+}
+
+func logout(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie(un)
+	if err != nil {
+		http.Redirect(w, req, "/data", http.StatusSeeOther)
+		return
+	}
+	c.MaxAge = -1 // delete cookie
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
